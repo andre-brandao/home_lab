2a6ef90bc18f46509516c298f63668de70da09b1
andre-brandao <82166576+andre-brandao@users.noreply.github.com>
2025-08-24 14:51:08 -0300

    wip

--- sync.sh
+++ sync.sh
@@ -5,8 +5,6 @@ REMOTE_HOST="home-ubuntu"
 REMOTE_DIR="/home/deds/traefik"
 LOCAL_DIR="src/traefik"
 
-echo "Creating remote directory if it doesn't exist..."
-ssh ${REMOTE_USER}@${REMOTE_HOST} "mkdir -p ${REMOTE_DIR}"
 
 echo "Copying ${LOCAL_DIR} files to ${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_DIR}..."
 rsync -avz --progress ${LOCAL_DIR}/ ${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_DIR}/
