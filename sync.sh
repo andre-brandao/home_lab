5ee2512d1753538725f95cbb3c515dfabdb13bbd
andre-brandao <82166576+andre-brandao@users.noreply.github.com>
2025-08-28 09:53:06 -0300

    fix monitoring

--- sync.sh
+++ sync.sh
@@ -2,12 +2,12 @@
 
 REMOTE_USER="deds"
 REMOTE_HOST="home-ubuntu"
-REMOTE_DIR="/home/deds/monitoring"
-LOCAL_DIR="src/monitoring"
+REMOTE_DIR="/home/deds/traefik"
+LOCAL_DIR="src/traefik"
 
 
 echo "Copying ${LOCAL_DIR} files to ${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_DIR}..."
-rsync -avz --progress ${LOCAL_DIR}/ ${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_DIR}/
+rsync -avz --progress  --ignore-existing=acme.json ${LOCAL_DIR}/ ${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_DIR}/
 
 
 echo "Synct completed successfully!"
