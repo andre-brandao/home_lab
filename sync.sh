d65a962057eddbc4117ac362dd4b24dd9bddabfb
andre-brandao <82166576+andre-brandao@users.noreply.github.com>
2025-08-28 13:56:00 -0300

    test vpn

--- sync.sh
+++ sync.sh
@@ -7,7 +7,10 @@ LOCAL_DIR="src/traefik"
 
 
 echo "Copying ${LOCAL_DIR} files to ${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_DIR}..."
-rsync -avz --progress  --ignore-existing=acme.json ${LOCAL_DIR}/ ${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_DIR}/
+rsync -avz --progress --exclude="acme.json" ${LOCAL_DIR}/ ${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_DIR}/
 
 
 echo "Synct completed successfully!"
+echo "Restarting docker compose..."
+ssh ${REMOTE_USER}@${REMOTE_HOST} "cd ${REMOTE_DIR} && docker compose down && docker compose up -d"
+echo "Docker compose restarted successfully!"
